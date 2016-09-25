d584f06ec9a9410cecbe09e48871124a60a6af76
Barrett Cervenka <barrett@cervenka.com>
2016-09-25 17:07:34 -0500

    If we are past the date of the outbound flight, don't check the trip prices anymore (purge from config file)

--- swa-price-drop.js
+++ swa-price-drop.js
@@ -8,6 +8,7 @@ const twilio = require('twilio');
 const winston = require('winston');
 const path = require('path');
 const os = require('os');
+const moment = require('moment');
 
 if (process.argv.length <= 2) {
   console.log('Usage: \n' +
@@ -84,127 +85,136 @@ const notify = (message) => {
 }
 
 const checkSouthwest = (flightConfig, cb) => {
-  const outboundFares = [];
-  const returnFares = [];
+  var now = moment();
+  var outboundDate = moment(flightConfig.outboundDate, "MM-DD-YYYY");
+  if (moment().isAfter(outboundDate)) {
+    // we are past the day of this outbound date so drop this trip from future checks
+    rewriteYamlConfig = true;
+    logger.info("Removing " + JSON.stringify(flightConfig) + " from future checks because it is past " + flightConfig.outboundDate);
+    cb();
+  } else {
+    const outboundFares = [];
+    const returnFares = [];
 
-  logger.info("Checking southwest.com for prices with parameters:\n" +
-    "originAirport: " + flightConfig.originAirport + "\n" +
-    "destinationAirport: " + flightConfig.destinationAirport + "\n" +
-    "outboundDateString: " + flightConfig.outboundDate + "\n" +
-    "returnDateString: " + flightConfig.returnDate + "\n" +
-    "adultPassengerCount: " + flightConfig.adultPassengerCount);
+    logger.info("Checking southwest.com for prices with parameters:\n" +
+      "originAirport: " + flightConfig.originAirport + "\n" +
+      "destinationAirport: " + flightConfig.destinationAirport + "\n" +
+      "outboundDateString: " + flightConfig.outboundDate + "\n" +
+      "returnDateString: " + flightConfig.returnDate + "\n" +
+      "adultPassengerCount: " + flightConfig.adultPassengerCount);
 
-  osmosis
-    .get("https://www.southwest.com")
-    .submit(".booking-form--form", {
-      twoWayTrip: true,
-      airTranRedirect: "",
-      returnAirport: "RoundTrip",
-      outboundTimeOfDay: "ANYTIME",
-      returnTimeOfDay: "ANYTIME",
-      seniorPassengerCount: 0,
-      fareType: "DOLLARS",
-      originAirport: flightConfig.originAirport,
-      destinationAirport: flightConfig.destinationAirport,
-      outboundDateString: flightConfig.outboundDate,
-      returnDateString: flightConfig.returnDate,
-      adultPassengerCount: flightConfig.adultPassengerCount
-    })
-    .set({
-      out: [
-        osmosis
-        .find("table[@id='faresOutbound']/tbody/tr")
-        .then((outboundData) => {
-          const flights = outboundData.find(".js-flight-performance");
+    osmosis
+      .get("https://www.southwest.com")
+      .submit(".booking-form--form", {
+        twoWayTrip: true,
+        airTranRedirect: "",
+        returnAirport: "RoundTrip",
+        outboundTimeOfDay: "ANYTIME",
+        returnTimeOfDay: "ANYTIME",
+        seniorPassengerCount: 0,
+        fareType: "DOLLARS",
+        originAirport: flightConfig.originAirport,
+        destinationAirport: flightConfig.destinationAirport,
+        outboundDateString: flightConfig.outboundDate,
+        returnDateString: flightConfig.returnDate,
+        adultPassengerCount: flightConfig.adultPassengerCount
+      })
+      .set({
+        out: [
+          osmosis
+          .find("table[@id='faresOutbound']/tbody/tr")
+          .then((outboundData) => {
+            const flights = outboundData.find(".js-flight-performance");
 
-          // Loop through all the outbound flights and find the flight number
-          // we are interested in
-          for (let flight of flights) {
-            const matches = flight.text().match(/\d+/);
-            const flightNumber = matches[0];
+            // Loop through all the outbound flights and find the flight number
+            // we are interested in
+            for (let flight of flights) {
+              const matches = flight.text().match(/\d+/);
+              const flightNumber = matches[0];
 
-            // we found the right flight number row - parse the prices for this row
-            if (flightNumber == flightConfig.outboundFlightNumber) {
-              const prices = outboundData.find(".product_price");
-              for (let rawPrice of prices) {
-                const priceMatch = rawPrice.toString().match(/\$.*?(\d+)/);
-                const price = parseInt(priceMatch[1]);
+              // we found the right flight number row - parse the prices for this row
+              if (flightNumber == flightConfig.outboundFlightNumber) {
+                const prices = outboundData.find(".product_price");
+                for (let rawPrice of prices) {
+                  const priceMatch = rawPrice.toString().match(/\$.*?(\d+)/);
+                  const price = parseInt(priceMatch[1]);
 
-                logger.debug("Found price " + price + " for outbound flight " + flightConfig.outboundFlightNumber);
-                outboundFares.push(price);
+                  logger.debug("Found price " + price + " for outbound flight " + flightConfig.outboundFlightNumber + " on " + flightConfig.outboundDate);
+                  outboundFares.push(price);
+                }
               }
             }
-          }
-        })
-      ]
-    })
-    .set({
-      return: [
-        osmosis
-        .find("table[@id='faresReturn']/tbody/tr")
-        .then((returnData) => {
-          const flights = returnData.find(".js-flight-performance");
+          })
+        ]
+      })
+      .set({
+        return: [
+          osmosis
+          .find("table[@id='faresReturn']/tbody/tr")
+          .then((returnData) => {
+            const flights = returnData.find(".js-flight-performance");
 
-          // Loop through all the return flights and find the flight number
-          // we are interested in
-          for (let flight of flights) {
-            const matches = flight.text().match(/\d+/);
-            const flightNumber = matches[0];
+            // Loop through all the return flights and find the flight number
+            // we are interested in
+            for (let flight of flights) {
+              const matches = flight.text().match(/\d+/);
+              const flightNumber = matches[0];
 
-            // we found the right flight number row - parse the prices for this row
-            if (flightNumber == flightConfig.returnFlightNumber) {
-              const prices = returnData.find(".product_price");
-              for (let rawPrice of prices) {
-                const priceMatch = rawPrice.toString().match(/\$.*?(\d+)/);
-                const price = parseInt(priceMatch[1]);
+              // we found the right flight number row - parse the prices for this row
+              if (flightNumber == flightConfig.returnFlightNumber) {
+                const prices = returnData.find(".product_price");
+                for (let rawPrice of prices) {
+                  const priceMatch = rawPrice.toString().match(/\$.*?(\d+)/);
+                  const price = parseInt(priceMatch[1]);
 
-                logger.debug("Found price " + price + " for return flight " + flightConfig.returnFlightNumber);
-                returnFares.push(price);
+                  logger.debug("Found price " + price + " for return flight " + flightConfig.returnFlightNumber + " on " + flightConfig.returnDate);
+                  returnFares.push(price);
+                }
               }
             }
-          }
-        })
-      ]
-    })
-    .done(() => {
-      const outboundPrice = parseInt(flightConfig.outboundPrice);
-      const returnPrice = parseInt(flightConfig.returnPrice);
-      const lowestOutboundFare = Math.min(...outboundFares);
-      const lowestReturnFare = Math.min(...returnFares);
-      logger.debug('Lowest outbound price for flight number ' + flightConfig.outboundFlightNumber + ' is ' + lowestOutboundFare);
-      logger.debug('Configured outbound price for flight ' + flightConfig.outboundFlightNumber + ' is ' + outboundPrice);
-      logger.debug('Lowest return price for flight number ' + flightConfig.returnFlightNumber + ' is ' + lowestReturnFare);
-      logger.debug('Configured return price for flight ' + flightConfig.returnFlightNumber + ' is ' + returnPrice);
+          })
+        ]
+      })
+      .done(() => {
+        const outboundPrice = parseInt(flightConfig.outboundPrice);
+        const returnPrice = parseInt(flightConfig.returnPrice);
+        const lowestOutboundFare = Math.min(...outboundFares);
+        const lowestReturnFare = Math.min(...returnFares);
+        logger.debug('Lowest outbound price for flight number ' + flightConfig.outboundFlightNumber + ' is ' + lowestOutboundFare);
+        logger.debug('Notification threshold outbound price for flight ' + flightConfig.outboundFlightNumber + ' is ' + outboundPrice);
+        logger.debug('Lowest return price for flight number ' + flightConfig.returnFlightNumber + ' is ' + lowestReturnFare);
+        logger.debug('Notification threshold return price for flight ' + flightConfig.returnFlightNumber + ' is ' + returnPrice);
 
-      if (lowestOutboundFare < outboundPrice) {
-        const message = "Price Drop: Outbound flight #" + flightConfig.outboundFlightNumber + " " +
-          flightConfig.originAirport + "->" + flightConfig.destinationAirport +
-          " on " + flightConfig.outboundDate +
-          " is now $" + lowestOutboundFare + " (was $" + flightConfig.outboundPrice + ")";
-        notify(message);
+        if (lowestOutboundFare < outboundPrice) {
+          const message = "Price Drop: Outbound flight #" + flightConfig.outboundFlightNumber + " " +
+            flightConfig.originAirport + "->" + flightConfig.destinationAirport +
+            " on " + flightConfig.outboundDate +
+            " is now $" + lowestOutboundFare + " (was $" + flightConfig.outboundPrice + ")";
+          notify(message);
 
-        // update fare price in config
-        flightConfig.outboundPrice = lowestOutboundFare;
-        // flag to indicate we need to re-write yaml config
-        rewriteYamlConfig = true;
-      }
+          // update fare price in config
+          flightConfig.outboundPrice = lowestOutboundFare;
+          // flag to indicate we need to re-write yaml config
+          rewriteYamlConfig = true;
+        }
 
-      if (lowestReturnFare < returnPrice) {
-        const message = "Price Drop: Return flight #" + flightConfig.returnFlightNumber + " " +
-          flightConfig.destinationAirport + "->" + flightConfig.originAirport +
-          " on " + flightConfig.returnDate +
-          " is now $" + lowestReturnFare + " (was $" + flightConfig.returnPrice + ")";
-        notify(message);
+        if (lowestReturnFare < returnPrice) {
+          const message = "Price Drop: Return flight #" + flightConfig.returnFlightNumber + " " +
+            flightConfig.destinationAirport + "->" + flightConfig.originAirport +
+            " on " + flightConfig.returnDate +
+            " is now $" + lowestReturnFare + " (was $" + flightConfig.returnPrice + ")";
+          notify(message);
 
-        // update fare price in config
-        flightConfig.returnPrice = lowestReturnFare;
-        // flag to indicate we need to re-write yaml config
-        rewriteYamlConfig = true;
-      }
+          // update fare price in config
+          flightConfig.returnPrice = lowestReturnFare;
+          // flag to indicate we need to re-write yaml config
+          rewriteYamlConfig = true;
+        }
 
-      updatedFlights.push(flightConfig);
-      cb();
-    })
+        updatedFlights.push(flightConfig);
+        cb();
+      })
+  }
 }
 
 // main
